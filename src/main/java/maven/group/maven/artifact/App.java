15e8d154698fbd123a0c618d6691b3b7075c68ba
edugithub <bf_eduado@yahoo.es>
2016-01-28 23:32:43 +0100

    inicio de proyecto maven

---
+++ src/main/java/maven/group/maven/artifact/App.java
@@ -0,0 +1,13 @@
+package maven.group.maven.artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
